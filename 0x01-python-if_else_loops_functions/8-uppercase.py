ce504c02de10015b094b0a1c5191168d3ce5c537
Emmanuel Adje Frimpong <tydevelopes@gmail.com>
2023-05-09 20:52:30 +0000

    fixed error in task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,7 @@ def uppercase(str):
     ch = ""
     while i < len(str):
         if ord(str[i]) >= 97 and ord(str[i]) <= 122:
-            ch = chr(ord(str[i] + 32))
+            ch = chr(ord(str[i]) + 32)
         else:
             ch = str[i]
         print("{0}".format(ch), end="")

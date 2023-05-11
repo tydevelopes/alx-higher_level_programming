dd46dbad0fc0a8b115598595ebb06185a1b4b93d
Emmanuel Adje Frimpong <tydevelopes@gmail.com>
2023-05-11 06:32:57 +0000

    added tasks 0 to 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    args = len(sys.argv) - 1
+
+    print(f"{args} {'argument' if args == 1 else 'arguments'}", end="")
+    print(f"{'.' if args == 0 else ':'}")

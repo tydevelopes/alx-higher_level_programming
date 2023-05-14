5af212fad95af059c2d89fabd0bfb94f1a4f036f
Emmanuel Adje Frimpong <tydevelopes@gmail.com>
2023-05-14 19:48:44 +0000

    fixed error in task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) == 0:
+    if not my_list:
         return
     rev = my_list[:]
     rev.reverse()
     for item in rev:
-        print("{}".format(item))
+        print("{:d}".format(item))

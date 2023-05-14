fe3129327a1e384e08b2a92bb426ede5a757d04b
Emmanuel Adje Frimpong <tydevelopes@gmail.com>
2023-05-14 20:56:36 +0000

    fixed error in task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
     no_c_str = ""
-    if not my_string:
-        return
     for ch in my_string:
         if ch != "c" and ch != "C":
             no_c_str += ch

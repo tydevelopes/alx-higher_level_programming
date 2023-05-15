ce318c9536647cbc46dcb57916c679d36a43bb0f
Emmanuel <tydevelopes@gmail.com>
2023-05-15 00:47:52 -0700

    added prototypes for task 14

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,11 @@
+#include <ctypes.h>
+#include <stdio.h>
+
+/**
+ * print_python_list_info - print python list info
+ * @p: pointer to python object
+ */
+void print_python_list_info(PyObject *p)
+{
+	printf("TODO");
+}

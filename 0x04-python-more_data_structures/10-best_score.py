fd18c3ffda39d8b97dfb3fbe225700b052704edb
Emmanuel Frimpong <tydevelopes@gmail.com>
2023-05-17 10:46:40 -0400

    fixed error in task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    best_key = a_dictionary.keys()[0]
+    best_key = list(a_dictionary.keys())[0]
     max = a_dictionary[best_key]
     for key, value in a_dictionary.items():
         if value > max:

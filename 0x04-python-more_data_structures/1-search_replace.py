486292caaae7a42d9665fb5563dd455c3e3ad72f
Emmanuel Frimpong <tydevelopes@gmail.com>
2023-05-17 09:09:12 -0400

    fixed error in task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    # list(map(lambda x : 89 if x == 2 else x, my_list))
-    return [89 if x == 2 else x for x in my_list]
+    # list(map(lambda x : replace if x == search else x, my_list))
+    return [replace if x == search else x for x in my_list]

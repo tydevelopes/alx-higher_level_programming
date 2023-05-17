92dbac2396f131719f92fbe14deb90f2dca42217
Emmanuel Frimpong <tydevelopes@gmail.com>
2023-05-17 12:24:22 -0400

    fixed error in task 15

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -6,7 +6,8 @@ def complex_delete(a_dictionary, value):
             exist = True
     if not exist:
         return a_dictionary
-    for key, val in a_dictionary.items():
+    pairs = list(a_dictionary.items())
+    for key, val in pairs:
         if val == value:
             del a_dictionary[key]
     return a_dictionary

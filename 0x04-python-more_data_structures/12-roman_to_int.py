27253385ca40bfc9210b18556d25009c4680c983
Emmanuel <tydevelopes@gmail.com>
2023-05-17 17:38:56 -0700

    fixed error in task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     romans = {'I': 1, 'IV': 4, 'V': 5, 'IX': 9, 'X': 10, 'XL': 40,
-              'L': 50, 'XC': 90, 'C': 100, 'CD': 400, 'D': 500, 'CM': 900, 'M': 1000}
+              'L': 50, 'XC': 90, 'C': 100, 'CD': 400,
+              'D': 500, 'CM': 900, 'M': 1000}
     if not roman_string or (not type(roman_string) is str):
         return 0
     ans = 0
+    temp = roman_string.upper()
 
-    for ch in roman_string.upper():
-        ans += romans[ch]
+    while (temp):
+        for key, value in romans:
+            if temp.startswith(key):
+                temp = temp.replace(key, "", 1)
+                ans += value
     return ans

646cecec0aee5d208908fd7bab8b0c471953d9f9
Emmanuel Frimpong <tydevelopes@gmail.com>
2023-05-29 20:32:31 -0400

    added task 8

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def safe_function(fct, *args):
+    result = None
+    try:
+        result = fct(*args)
+    except Exception as err:
+        print(f"Exception: {err}", file=sys.stderr)
+        return None
+    else:
+        return result

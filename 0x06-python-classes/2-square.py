a3de01d9f1ae306ab132e560761bbe7036c5de8c
Emmanuel Frimpong <tydevelopes@gmail.com>
2023-05-30 16:21:17 -0400

    added task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Module defines a square class """
+
+
+class Square:
+    """Defines a sqaure class"""
+
+    def __init__(self, size=0):
+        """Initialises class instance"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size

79745e5bbd1445666d6854d0bdefada19041129f
Emmanuel Frimpong <tydevelopes@gmail.com>
2023-05-29 20:09:48 -0400

    added task 7

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except Exception as err:
+        print(f"Exception: {err}", file=sys.stderr)
+        return False
+    else:
+        return True

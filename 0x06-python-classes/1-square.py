477b4179f920edd0540203fc5692e8ba88122ae2
Emmanuel Frimpong <tydevelopes@gmail.com>
2023-05-30 15:53:28 -0400

    added task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Module defines a square class """
+
+
+class Square:
+    """Defines a sqaure class"""
+
+    def __init__(self, size):
+        """Initialises class instance"""
+        self.__size = self

0b8364112f5352b6791ed5ddaf249824f7ea2e08
Emmanuel Frimpong <tydevelopes@gmail.com>
2023-05-30 22:39:27 -0400

    fixed error in task 8

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -70,11 +70,11 @@ class Square:
 
         string = ""
         if self.__size == 0:
-            return string
+            string += "\n"
         else:
             for i in range(self.__position[1]):
                 string += "\n"
             for i in range(self.__size):
                 string += f"{self.__position[0] * ' '}"
                 string += f"{self.__size * '#'}\n"
-            return string
+        return string

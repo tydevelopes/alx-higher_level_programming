7712489b39f98c432f5e41937d73deffecc930e5
Emmanuel Frimpong <tydevelopes@gmail.com>
2023-06-12 21:51:51 -0400

    fixed errors in task 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -11,9 +11,9 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         """Initialises an instance"""
 
-        BaseGeometry.integer_validator("width", width)
+        super().integer_validator("width", width)
         self.__width = width
-        BaseGeometry.integer_validator("height", height)
+        super().integer_validator("height", height)
         self.__height = height
 
     def area(self):
